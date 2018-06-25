384dd82cfa25b08da0938799cf5eb4e13996dc03
Annie Shin and Reese Glasscock <annie@>
2018-06-25 11:08:39 -0700

    test two successful with pass of if divisble by 3 it will then push ping

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,9 +1,13 @@
 def ping_pong(number)
-  number = 0
+  x = 0
   new_array = []
-  while (number < 3)
-  number += 1
-  new_array.push(number)
+  while (x < number)
+    x += 1
+    if x % 3 != 0
+      new_array.push(x)
+    elsif x % 3 == 0
+      new_array.push("ping")
+    end
   end
   new_array
 end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -4,7 +4,10 @@ require('ping_pong')
 
 
 describe('#ping_pong') do
-  it("outputs [1, 2, 3] when the user enters 2") do
-    expect(ping_pong(2)).to(eq([1, 2, 3]))
+  it("outputs [1, 2] when the user enters 2") do
+    expect(ping_pong(2)).to(eq([1, 2]))
+  end
+  it("outputs [1, 2, ping] when the user enters 3") do
+    expect(ping_pong(3)).to(eq([1,2,"ping"]))
   end
 end
